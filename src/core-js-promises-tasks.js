5428927fc66c58ea06f6673af676fba69319044a
dyoncha <dyoncha77@gmail.com>
2024-04-15 23:26:44 +0300

    feat: add getFirstResolvedPromiseResult function

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -63,8 +63,14 @@ function getPromiseResult(source) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with 1
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
-function getFirstResolvedPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstResolvedPromiseResult(promises) {
+  return new Promise((resolve, reject) => {
+    Promise.race(promises)
+      .then(resolve)
+      .catch(() => {
+        reject(new Error('All promises were rejected'));
+      });
+  });
 }
 
 /**
